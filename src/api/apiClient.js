52680f64218b1bd97265e023f0b0e2503804635b
Ujjwal Kumar <ujjwal.nrt@gmail.com>
2026-09-24 00:14:41 +0530

    fixed env

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,9 @@
-const API_URL = import.meta.env.API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const apiRequest = async (endpoint, options = {}) => {
   const token = localStorage.getItem("settle_token");
+
+  console.log({endpoint})
 
   const headers = {
     "Content-Type": "application/json",
